275486df2a5cd4e70b5c7359c4d64b0723023d46
cucker <hanxiao2100@qq.com>
2019-05-26 14:50:42 +0800

    修改 MaintenanceAndBugRecord fault_processing字段类型

--- apps/Core/models/maintenance_and_bugrecord.py
+++ apps/Core/models/maintenance_and_bugrecord.py
@@ -20,4 +20,4 @@ class MaintenanceAndBugRecord(BasicModel):
     start_time = models.DateTimeField(auto_now_add=True, verbose_name="故障/BUG开始时间")
     detaile = models.CharField(max_length=255, null=True, blank=True, verbose_name="故障/BUG祥情")
     type = models.CharField(max_length=32, verbose_name="故障/BUG类型")
-    fault_processing = models.CharField(max_length=512, null=True, blank=True, verbose_name="故障/BUG处理")
+    fault_processing = models.TextField(null=True, blank=True, verbose_name="故障/BUG处理")
