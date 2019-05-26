de38466541303f65e287fea02c2626ecb218d601
cucker <hanxiao2100@qq.com>
2019-05-26 11:44:56 +0800

    修改 BasicModel note字段的长度为512

--- apps/Core/common/basic_model.py
+++ apps/Core/common/basic_model.py
@@ -8,7 +8,7 @@ class BasicModel(models.Model):
     """
     基础Model
     """
-    note = models.CharField(max_length=255, null=True, blank=True, default="", verbose_name="备注")
+    note = models.CharField(max_length=512, null=True, blank=True, default="", verbose_name="备注")
     create_time = models.DateTimeField(auto_now_add=True, null=True, blank=True, verbose_name="创建时间")
     update_time = models.DateTimeField(auto_now=True, null=True, blank=True, verbose_name="更新时间")
 
